455fc8434fe06b774823362def9a01dbc48ba19a
peter krauss <ppkrauss@gmail.com>
2020-09-20 19:14:37 +0000

    rev

--- src/eclusa-step2a-ins.sh
+++ src/eclusa-step2a-ins.sh
@@ -1,2 +1,8 @@
-wget -c https://raw.githubusercontent.com/datasets-br/state-codes/master/data/br-state-codes.csv
-wget -O /tmp/br-region-codes.csv -c https://raw.githubusercontent.com/datasets-br/state-codes/master/data/br-region-codes.csv
+
+## nao usar -c pois nao atualizaria!
+
+wget -O /tmp/pg_io/br-state-codes.csv   https://raw.githubusercontent.com/datasets-br/state-codes/master/data/br-state-codes.csv
+wget -O /tmp/pg_io/br-region-codes.csv  https://raw.githubusercontent.com/datasets-br/state-codes/master/data/br-region-codes.csv
+
+wget -O /tmp/pg_io/br-donatedPack.csv    https://raw.githubusercontent.com/AddressForAll/digital-preservartion-BR/master/data/br-donatedPack.csv
+
